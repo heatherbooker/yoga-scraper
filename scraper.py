fa38b147045d319cacf902c5f63b7468bf36a972
Heather Booker <heather.booker@hotmail.com>
2017-03-21 23:21:45 -0700

    Scraper writes class data to file

--- scraper.py
+++ scraper.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 def init_driver():
 	driver = webdriver.Firefox()
@@ -11,31 +10,34 @@ def init_driver():
 	return driver
 
 def select_location(driver):
-        try:
-            location_selector = driver.wait.until(EC.element_to_be_clickable(
-                (By.NAME, "location")))
-            location_selector = Select(location_selector)
-            location_selector.select_by_visible_text("Richmond Olympic Oval")
-        except NoSuchElementException:
-            print("No such element found")
-            print("Element not visible")
-        except TimeoutException:
-            print("Timed out")
+        location_selector = driver.wait.until(EC.element_to_be_clickable(
+            (By.NAME, "location")))
+        location_selector = Select(location_selector)
+        location_selector.select_by_visible_text("Richmond Olympic Oval")
 
 def get_class_data(driver):
+    data = ''
     schedule = driver.wait.until(EC.visibility_of_element_located(
         (By.CLASS_NAME, "schedule")))
     rows = schedule.find_elements(By.TAG_NAME, "tr")
     for row in rows:
         cols = row.find_elements(By.TAG_NAME, "td")
         for col in cols:
-            print(col.text)
+            data += col.text
+            data += '\n'
+        data += "---new_row---\n"
+    return data
+
+def write_file(data):
+    out_file = open('yoga_classes.txt', 'w')
+    out_file.write(str(data))
+    out_file.close()
 
 if __name__ == "__main__":
     driver = init_driver()
     driver.get("https://www.yyoga.ca/bc/schedules")
     select_location(driver)
-    print("Everything seems to have gone well?")
-    get_class_data(driver)
-    time.sleep(50)
+    class_data = get_class_data(driver)
+    time.sleep(45)
     driver.quit()
+    write_file(class_data)
