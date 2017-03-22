d15a3dad82043a75af156b2c689bba2d46a054eb
Heather Booker <heather.booker@hotmail.com>
2017-03-22 11:01:34 -0700

    Add twilio

---
+++ twilio-client.js
@@ -0,0 +1,20 @@
+const twilio = require('twilio');
+const config = require('config');
+const yoga_manager = require('manager.js');
+
+
+// Twilio Credentials
+var accountSid = config.twilio_accountSid;
+var authToken = config.twilio_authToken;
+
+//require the Twilio module and create a REST client
+var client = twilio(accountSid, authToken);
+
+client.messages.create({
+    to: '+15197025387',
+    from: '+12262714596',
+    body: 'Morn: hot, cold, potato, so many yogas.',
+}, function (err, message) {
+    console.log(message.sid);
+});
+
