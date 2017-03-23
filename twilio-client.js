2e87488002fde29c8fb69c047334892eafc84ee3
Heather Booker <heather.booker@hotmail.com>
2017-03-22 21:57:35 -0700

    It texts you when you run twilio-client!

--- twilio-client.js
+++ twilio-client.js
@@ -1,19 +1,26 @@
+'use strict';
 const twilio = require('twilio');
-const config = require('config');
-const yoga_manager = require('manager.js');
+const yogaManager = require('./manager.js');
 
+// Make Twilio creds avail on process.env.
+require('dotenv').config();
+const accountSid = process.env.TWILIO_ID;
+const authToken = process.env.TWILIO_TOKEN;
 
-// Twilio Credentials
-var accountSid = config.twilio_accountSid;
-var authToken = config.twilio_authToken;
-
-//require the Twilio module and create a REST client
-var client = twilio(accountSid, authToken);
-
-client.messages.create({
-    to: config.phone_number,
-    from: '+12262714596',
-    body: 'Morn: hot, cold, potato, so many yogas.',
-}, function (err, message) {
-    console.log(message.sid);
+const classes = yogaManager.getClassesByTime();
+Object.keys(classes).forEach(time => {
+    if (classes.hasOwnProperty(time)) {
+        sendMsg(`${time}: ${classes[time]}`);
+    }
 });
+
+function sendMsg(msg) {
+    const client = twilio(accountSid, authToken);
+    client.messages.create({
+        to: process.env.PHONE_NUMBER,
+        from: '+12262714596',
+        body: msg,
+    }, function (err, message) {
+        console.log(message.sid);
+    });
+}
